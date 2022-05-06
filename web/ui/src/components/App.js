53c9b38934d9daa3d24b52de0b2678020fc79eec
ajgmez1 <ajgmez1@gmail.com>
2022-05-05 20:31:32 -0700

    update search fetch

--- web/ui/src/components/App.js
+++ web/ui/src/components/App.js
@@ -9,7 +9,6 @@ import '../styles/App.css';
 
 import Header from './Header';
 import Home from './home/Home';
-import About from './About';
 import Map from './jobs/Map';
 import Detail from './jobs/Detail';
 
@@ -23,16 +22,13 @@ class App extends Component {
             searching: true, 
             error: null 
         };
-
-        this.jobs = [];
     }
     
     componentDidMount() {
-        this.loopSearch('', 1);
+        this.search();
     }
 
-    onSearch = (e, searchVal, history) => {
-        searchVal = searchVal ? searchVal : '';
+    onSearch = (e, searchVal = '', history) => {
         if (e) e.preventDefault();
 
         this.setState({
@@ -44,52 +40,33 @@ class App extends Component {
         if (history.location.pathname !== '/jobs') {
             history.push('/jobs');
         }
-        this.jobs = [];
-        this.loopSearch(searchVal, 1);
-    }
-
-    // GitHub Job API is designed to be paged. We just want all results, 
-    // so let's fetch 6 at a time and keep going until we get empty
-    // results.
-    loopSearch(searchVal, page) {
-        let promises = [],
-            i;
         
-        for (i = page; i < page+6; i++) {
-            promises.push(this.search(searchVal, i));
-        }
-
-        Promise.all(promises).then((jobs) => {
-            // Any array was an empty result; we are done
-            if (jobs.find((j) => j && j.length === 0)) {
-                this.setState({ 
-                    searching: false, 
-                    error: null,
-                    jobs: this.jobs
-                });
-            } else {
-                this.loopSearch(searchVal, i);
-            }
-        }).catch((e) => {
-            this.setState({ 
-                searching: false, 
-                error: 'An error occured. Please search again or refresh your browser.' 
-            });
-        });;
+        this.search(searchVal);
     }
 
-    search(searchVal, page) {
-        let url = `/api/jobs?search=${searchVal}&page=${page}`;
+    search(v) {
+        let url = `/api/jobs`;
+        if (v) {
+            url += `?search=${v}`;
+        }
 
         return fetch(url)
             .then(r => r.json())
             .then((jobs) => {
                 if (jobs.error) {
-                    throw new Error(jobs.message);
+                    console.error(jobs.message);
+                    this.setState({
+                        searching: false,
+                        jobs: [],
+                        error: 'An error occurred.'
+                    });
+                } else {
+                    this.setState({ 
+                        searching: false, 
+                        error: null,
+                        jobs: jobs
+                    });
                 }
-
-                this.jobs = this.jobs.concat(jobs);
-                return jobs;
             });
     }
 
@@ -104,21 +81,22 @@ class App extends Component {
             <Router>
                 <React.Fragment>
                     <Header onSearch={this.onSearch} searching={this.state.searching} />
-                        
-                    <Route exact path="/" component={Home} />
-                    <Route path="/about" component={About} />
-
-                    <Route path="/job/:jobId" render={({match}) => 
-                        <Detail searching={this.state.searching} 
-                                job={this.state.jobs.find((j) => j.id === match.params.jobId)} />
-                    }/>
-
-                    <Route path="/jobs" render={() => 
+                    
+                    <Route exact path="/" render={() => 
                         <Map searching={this.state.searching}
                             setStateBounds={this.setBounds} 
                             bounds={this.state.bounds}
                             jobs={this.state.jobs} 
                             error={this.state.error} />
+                    }/>
+                    
+                    <Route path="/about" component={Home} />
+
+                    <Route path="/job/:jobId" render={({match}) => 
+                        <Detail 
+                            searching={this.state.searching} 
+                            job={this.state.jobs.find((j) => j.id === +match.params.jobId)} 
+                        />
                     }/>
 
                 </React.Fragment>
