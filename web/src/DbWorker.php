83d2ced8ff51b1046adbe8e526f9b51f3fef2f9c
ajgmez1 <ajgmez1@gmail.com>
2022-05-09 12:05:43 -0700

    remove url from Job model

--- web/src/DbWorker.php
+++ web/src/DbWorker.php
@@ -2,13 +2,12 @@
 
 class DbWorker {
 
+    const INSERT_SIZE = 2000;
     const URLS = [
         "OCCUPATIONS" => 'https://corpora-api.glitch.me/humans/occupations',
-        "URL" => 'https://random-data-api.com/api/internet_stuff/random_internet_stuff',
         "COMPANIES" => 'https://random-data-api.com/api/company/random_company',
         "LOREM" => 'https://random-data-api.com/api/lorem_ipsum/random_lorem_ipsum',
     ];
-    const INSERT_SIZE = 2000;
     
     private $db;
     private $rest;
@@ -68,7 +67,6 @@ class DbWorker {
             type varchar(100),
             company varchar(100),
             location varchar(100),
-            url varchar(100),
             description text,
             lat float,
             lng float,
@@ -78,7 +76,6 @@ class DbWorker {
 
     private function insert() {
         $result['OCCUPATIONS'] = $this->rest->get($this::URLS['OCCUPATIONS'])->decode_response();
-        $result['URL'] = $this->rest->get($this::URLS['URL'], [ "size" => 100 ])->decode_response();
         $result['COMPANIES'] = $this->rest->get($this::URLS['COMPANIES'], [ "size" => 100 ])->decode_response();
         $result['LOREM'] = $this->rest->get($this::URLS['LOREM'], [ "size" => 100 ])->decode_response();
         
@@ -88,23 +85,20 @@ class DbWorker {
             $occupations = $result["OCCUPATIONS"]->data->occupations;
             $lorem = $result["LOREM"][$i % sizeof($result["LOREM"])];
             $company = $result["COMPANIES"][$i % sizeof($result["COMPANIES"])];
-            $url = $result["URL"][$i % sizeof($result["URL"])];
 
-            array_push($data, 
-                [ 
-                    $occupations[rand(0, sizeof($occupations)-1)],
-                    $company->industry . ' - ' . $company->type,
-                    $company->business_name,
-                    $company->full_address,
-                    $url->url,
-                    sprintf('%s %s %s %s %s %s' ,$lorem->very_long_sentence, $lorem->long_sentence, $lorem->short_sentence, $lorem->paragraphs[0], $lorem->paragraphs[1], $lorem->paragraphs[2]),
-                    $company->latitude,
-                    $company->longitude
-                ]);
+            array_push($data, [ 
+                $occupations[rand(0, sizeof($occupations)-1)],
+                $company->industry . ' - ' . $company->type,
+                $company->business_name,
+                $company->full_address,
+                sprintf('%s %s %s %s %s %s' ,$lorem->very_long_sentence, $lorem->long_sentence, $lorem->short_sentence, $lorem->paragraphs[0], $lorem->paragraphs[1], $lorem->paragraphs[2]),
+                $company->latitude,
+                $company->longitude
+            ]);
         }
         
-        $this->query("insert into jobs (title, type, company, location, url, description, lat, lng, created_at)
-            values (?, ?, ?, ?, ?, ?, ?, ?, now())", $data);
+        $this->query("insert into jobs (title, type, company, location, description, lat, lng, created_at)
+            values (?, ?, ?, ?, ?, ?, ?, now())", $data);
     }
 }
 
